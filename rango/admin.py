2b04851c9dab54c21dd0c92e7329bbaac2c89502
Norah0x <2363680A@student.gla.ac.uk>
2019-02-12 01:59:48 +0000

    links_in_index_page_when_logged and links_in_index_page_when_logged Fix

--- rango/admin.py
+++ rango/admin.py
@@ -5,6 +5,9 @@ from rango.models import UserProfile
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
 admin.site.register(Category)
 admin.site.register(Page, PageAdmin) 
 admin.site.register(UserProfile)
